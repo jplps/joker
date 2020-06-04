5d8065fae36adc7b4fc11b46614551d3d6b5cc6d
jplps <joao.pedro.pacheco@hotmail.com>
2020-06-04 16:23:29 -0300

    Implemented fullstack successfuly

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api';
 import './App.css';
 
-function App() {
+interface Joke {
+  id: number;
+  text: string;
+}
+
+const App = () => {
+  const [jokes, setJokes] = useState<Joke[]>([]);
+
+  useEffect(() => {
+    api.get('/jokes')
+      .then(response => {
+        const jokesDB = response.data;
+        console.log(jokesDB);
+        setJokes(jokesDB);
+      });
+  }, []);
+
   return (
-    <h1>JokeR</h1>
+    <>
+      <h1>JokeR</h1>
+
+      {jokes.map(joke => (
+        <p key={joke.id}>{joke.text}</p>
+      ))}
+    </>
   );
 }
 
