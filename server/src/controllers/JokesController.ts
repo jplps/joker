488583bd251bf98b48c554f6ab91b01ea7206e74
jplps <joao.pedro.pacheco@hotmail.com>
2020-06-04 16:07:47 -0300

    Implemented controller

--- server/src/controllers/JokesController.ts
+++ server/src/controllers/JokesController.ts
@@ -1,14 +1,28 @@
 import { Request, Response } from 'express';
 import knex from '../database/connection';
 
-class LabsController {
+class JokesController {
 	async index(req: Request, res: Response) {
-		// index
+		try {
+			const jokes = await knex('jokes').select('*');
+
+			return res.status(200).json(jokes);
+		} catch (err) {
+			return res.status(400).json({ err });
+		}
 	}
 
 	async create(req: Request, res: Response) {
-		// create
+		const { text } = req.body;
+
+		try {
+			const joke = await knex('jokes').insert({ text });
+
+			return res.status(200).json(joke);
+		} catch (err) {
+			return res.status(400).json({ err });
+		}
 	}
 }
 
-export default LabsController;
+export default JokesController;
